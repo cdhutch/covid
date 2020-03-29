8cd104829de53cbf641e0d13b778e096b95ddea4
Craig Hutchinson <cdhutch@alumni.duke.edu>
2020-03-29 14:54:42 -0400

    Added US stats to graph

--- covid.py
+++ covid.py
@@ -5,7 +5,9 @@ import numpy as np
 state_url = 'https://covidtracking.com/api/states/daily.csv'
 us_url = 'https://covidtracking.com/api/us/daily.csv'
 df = pd.read_csv(state_url)
+df_usa = pd.read_csv(us_url)
 SUFFIXES = {1: 'st', 2: 'nd', 3: 'rd'}
+starting_caseload = 1
 
 
 def ordinal(num):
@@ -17,6 +19,19 @@ def ordinal(num):
         # the second parameter is a default.
         suffix = SUFFIXES.get(num % 10, 'th')
     return str(num) + suffix
+
+
+def modify_df(df, starting_caseload, stat):
+    pd.set_option("mode.chained_assignment", None)
+    df['NewDate'] = pd.to_datetime(
+        df['date'].copy(), format='%Y%m%d')
+    starting_caseload = 1
+    start_date = df[(df[stat]) >
+                    starting_caseload]['NewDate'].min()
+    df['date_zero'] = (df['NewDate'] -
+                       start_date).astype('timedelta64[D]')
+    pd.set_option("mode.chained_assignment", 'warn')
+    return df
 
 
 df_grid = pd.DataFrame(index=np.arange(0, 100, 0.25))
@@ -33,25 +48,27 @@ for stat in ['positive', 'death']:
     x_max, y_max = 0, 0
     for state in ['VA', 'NY', 'WA', 'CA']:
         df_plot = df[df['state'] == state]
-        pd.set_option("mode.chained_assignment", None)
-        df_plot['NewDate'] = pd.to_datetime(
-            df_plot['date'].copy(), format='%Y%m%d')
-        starting_caseload = 1
-        start_date = df_plot[(df_plot[stat]) >
-                             starting_caseload]['NewDate'].min()
-        df_plot['date_zero'] = (df_plot['NewDate'] -
-                                start_date).astype('timedelta64[D]')
-        pd.set_option("mode.chained_assignment", 'warn')
+        df_plot = modify_df(df_plot, starting_caseload, stat)
         ax.semilogy(df_plot['date_zero'], df_plot[stat], label=state)
         if df_plot[stat].max() > y_max:
             y_max = df_plot[stat].max()
         if df_plot['date_zero'].max() > x_max:
             x_max = df_plot['date_zero'].max()
+    df_usa = modify_df(df_usa, starting_caseload, stat)
+    ax.semilogy(df_usa['date_zero'], df_usa[stat],
+                'k', linewidth=2, label='Total US')
+    if df_usa[stat].max() > y_max:
+        y_max = df_usa[stat].max()
+    if df_usa['date_zero'].max() > x_max:
+        x_max = df_usa['date_zero'].max()
+
     ax.set_ylim(top=y_max, bottom=1)
     ax.set_xlim(left=0, right=x_max)
     ax.legend()
     str_xaxis_label = 'Days since {:s} {:s}'.format(
         ordinal(starting_caseload), stat)
     ax.set_xlabel(str_xaxis_label)
+    str_yaxis_label = 'Total number of {:s}s'.format(stat)
+    ax.set_ylabel(str_yaxis_label)
     ax.yaxis.set_major_formatter(ScalarFormatter())
 plt.show()
